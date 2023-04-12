4332a8493ad9e55fa6186f48515f7be481d5ea03
Tahir Mammadov <tmammadovt@gmail.com>
2023-04-12 17:23:04 +0400

    13 updated & added 15

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Clear();
+Console.Write($"Введите любое число: ");
+double num = Convert.ToDouble(Console.ReadLine());
+
+if(num<0)
+{
+    num=-num;
+}
+
+if (num >= 100)
+{
+    for (int i = 0; num >= 1000; i++)
+    {
+        num /= 10;
+    }
+    Console.WriteLine($"Результат: {num % 10 - num%1}");
+}
+else if (num < 100)
+{
+    Console.WriteLine($"третьей цифры нет");
+}
---
+++ Task15/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Clear();
+Console.Write($"Введите цифру от 1-го до 7-ми: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+while (num < 1 || num > 7)
+{
+    Console.Write($"неправильное значение!\nВведите цифру от 1-го до 7-ми: ");
+    num = Convert.ToInt32(Console.ReadLine());
+}
+
+//1-st option (if)
+if(num == 6 || num == 7)
+{
+    Console.WriteLine($"да, выходной день");
+}
+else
+{
+    Console.WriteLine($"нет");
+}
+
+//2-nd option (switch)
+// switch (num)
+// {
+//     case 6:
+//     Console.WriteLine("да, выходной день");
+//     break;
+
+//     case 7:
+//     Console.WriteLine("да, выходной день");
+//     break;
+
+//     default:
+//     Console.WriteLine("нет");
+//     break;
+// }
